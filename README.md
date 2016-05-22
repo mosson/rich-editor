1c49d064c8ff467166133272f051c18c887ac46f
mosson <cucation@gmail.com>
2016-05-22 17:12:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+node >= v4.2.2
+
+### Prepare
+
+```
+$ npm install
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+### Dev
+
+```
+$ npm run watch [-- -e sample]
+```
+
+### Test
+
+```
+$ npm test
+```
---
+++ src/javascripts/constants/common/constant.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Environment = {
+  ENTRY_ENDPOINT: '/entries',
+
+  ACTIONS: {
+    UPDATE : 'Common::Actions::Update',
+    DESTROY: 'Common::Actions::Destroy'
+  }
+};
+
+export default Environment;
